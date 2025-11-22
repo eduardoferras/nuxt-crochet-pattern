21640069cab39224d0a279a9e262a61b142f6a82
Eduardo Ferras <edferras@gmail.com>
2025-11-21 23:25:04 -0300

    feat(health): add Redis check to prevent service sleep mode

--- backend/src/routes/public/health.routes.ts
+++ backend/src/routes/public/health.routes.ts
@@ -1,7 +1,21 @@
+import redisConnection from "@config/redis.ts";
 import { Router } from "express";
 
 const router = Router();
 
-router.get("/", (req, res) => res.status(200).json({ status: "ok" }));
+router.get("/", async (_req, res) => {
+	try {
+		const redisStatus = await redisConnection.ping();
+		if (redisStatus) {
+			return res.sendStatus(200);
+		} else {
+			return res.status(500).json({ message: "Redis connection failed" });
+		}
+	} catch (error) {
+		console.error("Health check failed:", error);
+
+		return res.status(500).json({ message: "Health check failed", error });
+	}
+});
 
 export default router;
