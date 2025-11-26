73f0469de68b2e6c27f29333c994573e221d0659
Eduardo Ferras <edferras@gmail.com>
2025-11-25 22:51:48 -0300

    chore(config): rename api env

--- apps/frontend/nuxt.config.ts
+++ apps/frontend/nuxt.config.ts
@@ -12,7 +12,7 @@ export default defineNuxtConfig({
 	},
 	runtimeConfig: {
 		public: {
-			apiUrl: process.env.NUXT_PUBLIC_API_URL,
+			apiUrl: process.env.API_URL,
 			whatsAppSales: '',
 			gtm: {
 				id: process.env.NUXT_PUBLIC_GTM_ID as string,
