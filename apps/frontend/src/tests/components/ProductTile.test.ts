e6bd2345a1304eaf6aaa6f9b2d1e549251df2c99
Eduardo Ferras <edferras@gmail.com>
2025-12-13 00:20:38 -0300

    fix(tests): whatsApp Button

--- apps/frontend/src/tests/components/ProductTile.test.ts
+++ apps/frontend/src/tests/components/ProductTile.test.ts
@@ -1,7 +1,10 @@
-import { describe, expect, it } from 'vitest'
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 import { mockNuxtImport, mountSuspended } from '@nuxt/test-utils/runtime'
 import ProductTile from '@components/ProductTile.vue'
 import mockProducts from '@/tests/mocks/products.mock'
+import type { VueWrapper } from '@vue/test-utils'
+
+const trackEventSpy = vi.fn()
 
 mockNuxtImport('useFetch', () => {
 	return async () => ({
@@ -9,18 +12,33 @@ mockNuxtImport('useFetch', () => {
 	})
 })
 
+mockNuxtImport('useGtm', () => {
+	return () => ({
+		trackEvent: trackEventSpy
+	})
+})
+
+const findWhatsAppBtn = (wrapper: VueWrapper) => wrapper.find('button.btn--whatsapp')
+
+let wrapper: VueWrapper
+
+beforeEach(async () => {
+	wrapper = await mountSuspended(ProductTile)
+})
+
+afterEach(() => {
+	vi.restoreAllMocks()
+	trackEventSpy.mockClear()
+})
+
 describe('ProductTile', () => {
 	it('should render products correctly', async () => {
-		const wrapper = await mountSuspended(ProductTile)
-
 		const items = wrapper.findAll('[data-test="product-card"]')
 
 		expect(items).toHaveLength(2)
 	})
 
 	it('should render price with two decimal places', async () => {
-		const wrapper = await mountSuspended(ProductTile)
-
 		const priceElements = wrapper.findAll('[data-test="product-price"]')
 
 		priceElements.forEach((price) => {
@@ -31,29 +49,41 @@ describe('ProductTile', () => {
 		})
 	})
 
-	it('should render WhatsApp buy link correctly', async () => {
-		const wrapper = await mountSuspended(ProductTile)
+	it('should render WhatsApp button correctly', async () => {
+		const whatsAppBtn = findWhatsAppBtn(wrapper)
 
-		const items = wrapper.findAll('[data-test="product-card"]')
+		expect(whatsAppBtn.exists()).toBe(true)
+	})
 
-		items.forEach((item) => {
-			const link = item.find('a')
-			const href = link.attributes('href') as string
+	it('should dispatch gtm trackEvent event when WhatsApp button is clicked', async () => {
+		const whatsAppBtn = findWhatsAppBtn(wrapper)
 
-			expect(link.exists()).toBe(true)
-			expect(href).toContain('https://wa.me/')
+		await whatsAppBtn.trigger('click')
+
+		expect(trackEventSpy).toHaveBeenCalledWith({
+			event: 'generate_lead',
+			currency: 'BRL',
+			value: mockProducts[0].price
 		})
 	})
 
+	it('should open WhatsApp chat when WhatsApp button is clicked', async () => {
+		const whatsAppBtn = findWhatsAppBtn(wrapper)
+
+		const openSpy = vi.spyOn(window, 'open').mockImplementation(() => null)
+
+		await whatsAppBtn.trigger('click')
+
+		expect(openSpy).toHaveBeenCalledWith(expect.stringContaining('https://wa.me/'), '_blank')
+	})
+
 	it('should render a product image correctly', async () => {
-		const wrapper = await mountSuspended(ProductTile)
+		const product = wrapper.find('[data-test="product-card"]')
 
-		const items = wrapper.findAll('[data-test="product-card"]')
+		const productImage = product.find('img')
 
-		items.forEach((item) => {
-			const productImage = item.find('img')
-
-			expect(productImage.exists()).toBe(true)
-		})
+		expect(productImage.exists()).toBe(true)
+		expect(productImage.attributes('src')).toBeTruthy()
+		expect(productImage.attributes('alt')).toBe(mockProducts[0].name)
 	})
 })
