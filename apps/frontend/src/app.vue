3136beed438cb6445ef1f21ed9e9731b7d689ab2
Eduardo Ferras <edferras@gmail.com>
2026-01-22 17:24:05 -0300

    add default layout

--- apps/frontend/src/app.vue
+++ apps/frontend/src/app.vue
@@ -1,28 +1,7 @@
 <template>
-	<div class="App">
-		<AppHeader />
-		<main class="App__main">
+	<div>
+		<NuxtLayout>
 			<NuxtPage />
-		</main>
-		<FeedbackWidget />
-		<AppFooter />
+		</NuxtLayout>
 	</div>
 </template>
-<script setup lang="ts">
-useHead({
-	titleTemplate: '%s | Receitas de Crochê'
-})
-</script>
-
-<style scoped lang="scss">
-.App {
-	display: flex;
-	flex-direction: column;
-	min-height: 100vh;
-
-	&__main {
-		flex: 1;
-		padding: 2.8rem 0;
-	}
-}
-</style>
---
+++ apps/frontend/src/layouts/default.vue
@@ -0,0 +1,28 @@
+<template>
+	<div class="default">
+		<AppHeader />
+		<main class="default-content">
+			<slot />
+		</main>
+		<FeedbackWidget />
+		<AppFooter />
+	</div>
+</template>
+<script setup lang="ts">
+useHead({
+	titleTemplate: '%s | Receitas de Crochê'
+})
+</script>
+
+<style lang="scss">
+.default {
+	display: flex;
+	flex-direction: column;
+	min-height: 100vh;
+
+	&-content {
+		flex: 1;
+		padding: 2.8rem 0;
+	}
+}
+</style>
