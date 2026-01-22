fd4a7e28080d51db741e8829e555a991b4eac7cc
Eduardo Ferras <edferras@gmail.com>
2026-01-22 16:50:05 -0300

    add all Roboto font weights to googleFonts config

--- apps/frontend/nuxt.config.ts
+++ apps/frontend/nuxt.config.ts
@@ -38,7 +38,7 @@ export default defineNuxtConfig({
 	],
 	googleFonts: {
 		families: {
-			Roboto: [400, 700]
+			Roboto: [100, 200, 300, 400, 500, 600, 700, 800, 900]
 		},
 		display: 'swap'
 	},
