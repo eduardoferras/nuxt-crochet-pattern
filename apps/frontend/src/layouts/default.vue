4440ea5307ce8adcb8b55e16f535250e74b2abe6
Eduardo Ferras <edferras@gmail.com>
2026-01-22 17:24:05 -0300

    refactor(web): migrate app.vue to use NuxtLayout component

---
+++ apps/frontend/src/layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+	<div class="default">
+		<AppHeader />
+		<main class="default-content">
+			<slot />
+		</main>
+		<FeedbackWidget />
+		<AppFooter />
+	</div>
+</template>
+
+<style lang="scss">
+.default {
+	display: flex;
+	flex-direction: column;
+	min-height: 100vh;
+
+	&-content {
+		flex: 1;
+		padding: 2.8rem 0;
+	}
+}
+</style>
