71cb2bc30f2a9af2b3120e6fee2403a708450e11
Eduardo Ferras <edferras@gmail.com>
2026-01-23 15:09:50 -0300

    chore(backend): add trustedOrigins to betterAuth config

--- apps/backend/src/config/better-auth.config.ts
+++ apps/backend/src/config/better-auth.config.ts
@@ -1,9 +1,11 @@
+import { env } from "@config/env.config.ts";
 import db from "@db/index.ts";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { jwt, openAPI } from "better-auth/plugins";
 
 export const auth = betterAuth({
+	trustedOrigins: [env.FRONTEND_URL],
 	database: drizzleAdapter(db, {
 		provider: "pg",
 		usePlural: true,
