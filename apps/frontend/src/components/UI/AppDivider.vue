4277f4cb5e4cb625b609bcf163da38dab9949437
Eduardo Ferras <edferras@gmail.com>
2026-01-28 17:01:50 -0300

    add AppDivider component

---
+++ apps/frontend/src/components/UI/AppDivider.vue
@@ -0,0 +1,11 @@
+<template>
+	<hr class="divider" />
+</template>
+
+<style scoped lang="scss">
+.divider {
+	border: #dfd1c380 1px solid;
+	border-width: 0;
+	border-top-width: 0.1rem;
+}
+</style>
