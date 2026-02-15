937bc41656b4a941cd7563b10565f7a07a907691
Eduardo Ferras <edferras@gmail.com>
2026-02-15 01:26:50 -0300

    build(backend): enable minify in tsup config

--- apps/backend/tsup.config.ts
+++ apps/backend/tsup.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "tsup";
 export default defineConfig({
 	entry: ["src/server.ts"],
 	noExternal: [/^@rdc\/.*/],
+	minify: true,
 	format: ["esm"],
 	target: "es2023",
 	clean: true,
